62c3cc645610383b59e4a0a9f627de6017a238b1
Babow Thamonwan <Babow@MacBook-Air--Konsuay.local>
2026-01-03 23:52:48 +0700

    chore: upload project from local computer (index, pages, catalog images, and script files)

---
+++ jss/index.js
@@ -0,0 +1,32 @@
+// function changeLanguage(lang) {
+//     const flag = document.getElementById('current-flag');
+//     const home = document.getElementById('nav_home');
+//     const about = document.getElementById('nav_about')
+//     const machine  = document.getElementById('nav_machine')
+//     const Boom = document.getElementById('nav_boom')
+//     const X = document.getElementById('nav_x')
+//     const News = document.getElementById('nav_news')
+//     const Seach = document.getElementById('nav_seach')
+
+//     if (lang === 'th') {
+//         flag.textContent = '🇹🇭';
+//         home.textContent = 'หน้าหลัก';
+//         about.textContent = 'เกี่ยวกับเรา';
+//         machine.textContent = 'เครื่องจักร'
+//         Boom.textContent = 'รถกระเช้า Boom Lift'
+//         X.textContent = 'ลิฟท์ขากรรไกร X-Lift'
+//         News.textContent = 'ข่าวสาร เเละประชาสัมพันธ์'
+//         Seach.textContent = 'ค้นหา'
+
+//     } else if (lang === 'en') {
+//         flag.textContent = '🇺🇸';
+//         home.textContent = 'Home';
+//         about.textContent = 'About';
+//         machine.textContent = 'Machine'
+//         Boom.textContent = 'Boom Lift'
+//         X.textContent = 'X-Lift'
+//         News.textContent = 'News'
+//         Seach.textContent = 'Seach'
+        
+//     }
+// }
